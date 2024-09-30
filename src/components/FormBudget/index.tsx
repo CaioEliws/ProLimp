5f4f8c8c3604c88ed357fb2f5d05245943bf1e33
caio <caioeliasvieira2@gmail.com>
2024-09-30 11:45:30 -0300

    Updated the form submission via email

--- src/components/FormBudget/index.tsx
+++ src/components/FormBudget/index.tsx
@@ -1,31 +1,71 @@
+import { FormEvent, useState } from "react";
 import { Button } from "../Button";
 import { Form, Title } from "./styles";
+
+import emailJs from "@emailjs/browser"
 
 interface FormBudgetProps {
     title: string;
 }
 
 export function FormBudget({ title }: FormBudgetProps) {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [tel, setTel] = useState('')
+    const [service, setService] = useState<string>('Limpeza');
+
+    function sendEmail(event: FormEvent) {
+        event.preventDefault();
+
+        const templateParams = {
+            from_name: name,
+            email: email,
+            tel: tel,
+            service: service,
+        }
+
+        emailJs.send("service_53kwuwe", "template_kh6ps97", templateParams, "ZzJTBA1rxuHC4Or-Y")
+        .then((response) => {
+            console.log("Email Enviado!", response.status, response.text)
+            setName('')
+            setEmail('')
+            setTel('')
+            setService('Limpeza');
+        }, (error) => {
+            console.log("ERROR: ", error)
+        })
+    }
+
     return (
-        <Form>
+        <Form onSubmit={sendEmail} >
             <Title>{ title }</Title>
 
             <input 
                 type="text" 
                 placeholder="Digite seu nome"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 required
             />
             <input 
                 type="email" 
                 placeholder="Digite seu e-mail"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
                 required
             />
             <input 
                 type="tel" 
                 placeholder="Digite seu Whatsapp"
+                onChange={(e) => setTel(e.target.value)}
+                value={tel}
                 required
             />
-            <select required>
+            <select
+                value={service} 
+                onChange={(e) => setService(e.target.value)} 
+                required
+            >
                 <option value="Limpeza">Limpeza</option>
                 <option value="Impermeabilização">Impermeabilização</option>
                 <option value="Limpeza Completa">Limpeza Completa</option>
--- src/components/FormContact/index.tsx
+++ src/components/FormContact/index.tsx
@@ -1,27 +1,65 @@
+import { FormEvent, useState } from "react";
 import { Form, FormSubmitButton } from "./styles";
 
+import emailJs from "@emailjs/browser"
+
 export function FormContact() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [tel, setTel] = useState('')
+    const [message, setMessage] = useState('')
+
+    function sendEmail(event: FormEvent) {
+        event.preventDefault();
+
+        const templateParams = {
+            from_name: name,
+            email: email,
+            tel: tel,
+            message: message,
+        }
+
+        emailJs.send("service_53kwuwe", "template_f0xlbxc", templateParams, "ZzJTBA1rxuHC4Or-Y")
+        .then((response) => {
+            console.log("Email Enviado!", response.status, response.text)
+            setName('')
+            setEmail('')
+            setTel('')
+            setMessage('');
+        }, (error) => {
+            console.log("ERROR: ", error)
+        })
+    }
+
     return (
-        <Form>
+        <Form onSubmit={sendEmail} >
             <input 
                 type="text" 
                 placeholder="Digite seu nome"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 required
             />
             <div>
                 <input 
                     type="email" 
                     placeholder="Digite seu e-mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
                     required
                 />
                 <input 
                     type="tel" 
                     placeholder="Digite seu Telefone"
+                    onChange={(e) => setTel(e.target.value)}
+                    value={tel}
                     required
-                />
+                    />
             </div>
             <textarea
                 placeholder="Digite a sua mensagem"
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
             />
 
             <FormSubmitButton 
